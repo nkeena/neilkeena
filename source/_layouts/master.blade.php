2f017c004a64f9a1fa37fb7070576319b7e201fc
Neil Keena <nkeena1@gmail.com>
2018-12-29 20:50:26 -0800

    initial commit

---
+++ source/_layouts/master.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
+        <link href="https://fonts.googleapis.com/css?family=Monoton" rel="stylesheet">
+    </head>
+    <body class="bg-grey-20 font-sans leading-normal">
+        <div class="h-3 bg-indigo-50" style="background-image: linear-gradient(to right, #98AEEB, #E0E8F9);"></div>
+        @yield('body')
+    </body>
+</html>
